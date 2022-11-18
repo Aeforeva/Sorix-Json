94c75455354785ed05c9981a96ba07350a1582e7
Aeforeva <83841383+Aeforeva@users.noreply.github.com>
2022-11-18 18:58:06 +0300

    images placeholder d/c err

--- app/src/main/java/com/example/sorixjson/BindingAdapters.kt
+++ app/src/main/java/com/example/sorixjson/BindingAdapters.kt
@@ -39,16 +39,20 @@ fun bindRecyclerView(recyclerView: RecyclerView, data: List<Applicable>?) {
 fun bindImage(imgView: ImageView, imgUrl: String?) {
     imgUrl?.let {
         val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
-        imgView.load(imgUri)
+        imgView.load(imgUri) {
+            placeholder(R.drawable.loading_animation)
+            error(R.drawable.ic_broken_image)
+        }
     }
 }
 
 /**
- * This binding adapter displays the [AmphibianApiStatus] of the network request in an image view.
+ * This binding adapter displays the [SorixApiStatus] of the network request in an image view.
  * When the request is loading, it displays a loading_animation.  If the request has an error, it
  * displays a broken image to reflect the connection error.  When the request is finished, it
  * hides the image view.
  */
+
 //@BindingAdapter("apiStatus")
 //fun bindStatus(statusImageView: ImageView, status: SorixApiStatus?) {
 //    when(status) {
